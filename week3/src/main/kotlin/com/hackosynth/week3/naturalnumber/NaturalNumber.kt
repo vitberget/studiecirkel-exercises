6900c567932a84e701ad5723876b29c2eceb89a9
Kenneth Hedman <github@vitberget.se>
2020-03-18 13:14:31 +0100

    NaturalNumber solution

--- week3/src/main/kotlin/com/hackosynth/week3/naturalnumber/NaturalNumber.kt
+++ week3/src/main/kotlin/com/hackosynth/week3/naturalnumber/NaturalNumber.kt
@@ -1,9 +1,27 @@
 package com.hackosynth.week3.naturalnumber
 
+import com.hackosynth.week3.naturalnumber.Classification.*
+
 enum class Classification {
-    DEFICIENT, PERFECT, ABUNDANT
+    DEFICIENT,
+    PERFECT,
+    ABUNDANT
 }
 
 object NaturalNumber {
-    fun classify(naturalNumber: Int): Classification = TODO()
+    fun classify(naturalNumber: Int): Classification {
+        require(naturalNumber > 0)
+
+        val mummy = mummify(naturalNumber)
+        return when {
+            mummy == naturalNumber -> PERFECT
+            mummy < naturalNumber -> DEFICIENT
+            else -> ABUNDANT
+        }
+    }
+
+    fun mummify(naturalNumber: Int): Int =
+        (1..(naturalNumber / 2))
+            .filter { naturalNumber % it == 0 }
+            .sum()
 }
