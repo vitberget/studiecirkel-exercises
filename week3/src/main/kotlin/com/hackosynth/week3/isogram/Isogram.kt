590b6d3592bc4ff0fc06f5577670fe483910be62
Kenneth Hedman <github@vitberget.se>
2020-03-18 11:48:40 +0100

    Isogram solution, 2 of them

--- week3/src/main/kotlin/com/hackosynth/week3/isogram/Isogram.kt
+++ week3/src/main/kotlin/com/hackosynth/week3/isogram/Isogram.kt
@@ -1,5 +1,37 @@
 package com.hackosynth.week3.isogram
 
 object Isogram {
-    fun isIsogram(input: String): Boolean = TODO()
+
+    // for fun
+    fun isIsogram(input: String): Boolean {
+        return input
+            .filter { it.isLetter() }
+            .fold(HashMap(), mappieFoldFun())
+            .filterValues { it > 1 }
+            .isEmpty()
+    }
+
+    private fun mappieFoldFun(): (HashMap<Char, Int>, Char) -> HashMap<Char, Int> =
+        { acc, char ->
+            acc.compute(char.toUpperCase()) { c, i -> (i ?: 0) + 1 };
+            acc
+        }
+
+    // for the need of speeeeed...
+    fun isIsogram2(input: String): Boolean {
+        val lettersSeen = HashSet<Char>()
+
+        input
+            .filter { it.isLetter() }
+            .forEach {
+                val upperCase = it.toUpperCase()
+                if (lettersSeen.contains(upperCase)) {
+                    return false
+                } else {
+                    lettersSeen.add(upperCase)
+                }
+            }
+
+        return true
+    }
 }
