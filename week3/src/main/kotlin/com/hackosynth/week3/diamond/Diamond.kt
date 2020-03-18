517a48efef1446cedd2a932f6a542ad7c3c54a9a
Kenneth Hedman <github@vitberget.se>
2020-03-18 13:50:47 +0100

    Diamond Solution

--- week3/src/main/kotlin/com/hackosynth/week3/diamond/Diamond.kt
+++ week3/src/main/kotlin/com/hackosynth/week3/diamond/Diamond.kt
@@ -1,5 +1,20 @@
 package com.hackosynth.week3.diamond
 
 object Diamond {
-    fun printToList(letter: Char): List<String> = TODO()
+    fun printToList(letter: Char): List<String> =
+        aToLetterToA(letter)
+            .map { line(letter, it) }
+
+    private fun aToLetterToA(letter: Char):List<Char> {
+        val aToLetter = 'A'..letter
+        return aToLetter + aToLetter.reversed().drop(1)
+    }
+
+    private fun line(letter: Char, current: Char): String {
+        val padding = " ".repeat(letter - current)
+        return when (current) {
+            'A' -> padding + current + padding
+            else -> padding + current + " ".repeat((current - 'A') * 2 - 1) + current + padding
+        }
+    }
 }
