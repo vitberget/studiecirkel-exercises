1e6bb7e92d55e911d88389c5e74b4f5f81f5cc4b
Kenneth Hedman <github@vitberget.se>
2020-04-01 00:27:12 +0200

    Forth solution, second edition

--- week3/src/main/kotlin/com/hackosynth/week3/forth/Forth.kt
+++ week3/src/main/kotlin/com/hackosynth/week3/forth/Forth.kt
@@ -1,6 +1,6 @@
 package com.hackosynth.week3.forth
 
-typealias STACK_FUN = (List<Int>) -> List<Int>
+typealias STACK_FUN = (List<Int>) -> List<Int> // Because so much easier to read :D
 
 object Forth {
     fun evaluate(vararg line: String): List<Int> {
@@ -17,42 +17,79 @@ object Forth {
 
         var stack = listOf<Int>()
 
-        line
-            .map { it.trim() }
+        line.map { it.trim() }
             .map { it.split("""\s+""".toRegex()) }
             .forEach { list ->
-                if (list.first() == ":") {
-                    val list2 = list.drop(1).dropLast(1)
-                    require(list2.size > 1)
-                    val word = list2.first().toLowerCase()
-
-                    if (word.toIntOrNull() != null) {
-                        require(false)
-                    }
-
-                    operators.put(
-                        word,
-                        userDefine(list2.drop(1).map { mapFun(operators, it) })
-                    )
-                } else {
-                    list.forEach {
-                        val num = it.toIntOrNull()
-                        if (num != null) {
-                            stack = stack + num
-                        } else {
-                            val op = operators.get(it.toLowerCase()) ?: ::throwError
-                            stack = op.invoke(stack)
-                        }
-                    }
-                }
+                if (list.first() == ":")
+                    doSomeUserDef(operators, list)
+                else
+                    stack = doSomeStackStuff(list, stack, operators)
             }
 
         return stack
     }
 
 
+// --- Stack manipulation
+
+    fun doSomeStackStuff(
+        list: List<String>,
+        originalStack: List<Int>,
+        operators: MutableMap<String, STACK_FUN>
+    ): List<Int> {
+
+        var stack = originalStack
+
+        list.forEach {
+            val num = it.toIntOrNull()
+            if (num != null) {
+                stack = stack + num
+            } else {
+                val op = operators.get(it.toLowerCase()) ?: ::throwError
+                stack = op.invoke(stack)
+            }
+        }
+
+        return stack
+    }
+
+
+// --- User define stuff belooooow....
+
+    fun doSomeUserDef(operators: MutableMap<String, STACK_FUN>, list: List<String>) {
+        val list2 = list.drop(1).dropLast(1) // drop : and ;
+        require(list2.size > 1)
+
+        val word = list2.first().toLowerCase()
+
+        if (word.toIntOrNull() != null) {
+            require(false)
+        }
+
+        operators[word] = list2.drop(1)
+            .map { mapFun(operators, it) }
+            .reduce { acc, fn -> { fn(acc(it)) } }
+    }
+
+    private fun mapFun(operators: MutableMap<String, STACK_FUN>, word: String): STACK_FUN {
+        val num = word.toIntOrNull()
+        return if (num != null) {
+            { it + num }
+        } else {
+            operators.get(word.toLowerCase()) ?: ::throwError
+        }
+    }
+
+    fun throwError(stack: List<Int>): List<Int> {
+        require(false);
+        return emptyList()
+    }
+
+
+// --- "Original" "Forth" funs belooooooooow....
+
     fun plus(stack: List<Int>): List<Int> {
-        require(stack.size == 2)
+        require(stack.size == 2) // Perhaps not a real requirement, but it passes the tests... so, if not it's a "feature"
         return listOf(stack[0] + stack[1])
     }
 
@@ -90,21 +127,5 @@ object Forth {
         require(stack.size >= 2)
         return stack + stack.takeLast(2).first()
     }
-
-    private fun mapFun(operators: MutableMap<String, STACK_FUN>, word: String): STACK_FUN {
-        val num = word.toIntOrNull()
-        return if (num != null) {
-             { it + num }
-        } else {
-            operators.get(word.toLowerCase())?: :: throwError
-        }
-    }
-
-     fun throwError(stack: List<Int>) : List<Int> {
-        require(false);
-        return emptyList()
-    }
-
-    fun userDefine(funs: List<STACK_FUN>): STACK_FUN = funs.reduce { acc, fn -> { fn(acc(it)) } }
 }
 
