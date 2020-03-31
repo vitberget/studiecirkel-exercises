31009965fc17237c7684888d74502dc88a6fa36d
Kenneth Hedman <github@vitberget.se>
2020-03-31 19:28:17 +0200

    Forth Solution, first edition

--- week3/src/main/kotlin/com/hackosynth/week3/forth/Forth.kt
+++ week3/src/main/kotlin/com/hackosynth/week3/forth/Forth.kt
@@ -1,5 +1,110 @@
 package com.hackosynth.week3.forth
 
+typealias STACK_FUN = (List<Int>) -> List<Int>
+
 object Forth {
-    fun evaluate(vararg line: String): List<Int> = TODO()
+    fun evaluate(vararg line: String): List<Int> {
+        val operators = mutableMapOf<String, STACK_FUN>(
+            "+" to ::plus,
+            "-" to ::minus,
+            "*" to ::multiply,
+            "/" to ::divide,
+            "dup" to ::dup,
+            "drop" to ::drop,
+            "swap" to ::swap,
+            "over" to ::over
+        )
+
+        var stack = listOf<Int>()
+
+        line
+            .map { it.trim() }
+            .map { it.split("""\s+""".toRegex()) }
+            .forEach { list ->
+                if (list.first() == ":") {
+                    val list2 = list.drop(1).dropLast(1)
+                    require(list2.size > 1)
+                    val word = list2.first().toLowerCase()
+
+                    if (word.toIntOrNull() != null) {
+                        require(false)
+                    }
+
+                    operators.put(
+                        word,
+                        userDefine(list2.drop(1).map { mapFun(operators, it) })
+                    )
+                } else {
+                    list.forEach {
+                        val num = it.toIntOrNull()
+                        if (num != null) {
+                            stack = stack + num
+                        } else {
+                            val op = operators.get(it.toLowerCase()) ?: ::throwError
+                            stack = op.invoke(stack)
+                        }
+                    }
+                }
+            }
+
+        return stack
+    }
+
+
+    fun plus(stack: List<Int>): List<Int> {
+        require(stack.size == 2)
+        return listOf(stack[0] + stack[1])
+    }
+
+    fun minus(stack: List<Int>): List<Int> {
+        require(stack.size == 2)
+        return listOf(stack[0] - stack[1])
+    }
+
+    fun multiply(stack: List<Int>): List<Int> {
+        require(stack.size == 2)
+        return listOf(stack[0] * stack[1])
+    }
+
+    fun divide(stack: List<Int>): List<Int> {
+        require(stack.size == 2)
+        return listOf(stack[0] / stack[1])
+    }
+
+    fun dup(stack: List<Int>): List<Int> {
+        require(stack.size >= 1)
+        return stack + stack.last()
+    }
+
+    fun drop(stack: List<Int>): List<Int> {
+        require(stack.size >= 1)
+        return stack.dropLast(1)
+    }
+
+    fun swap(stack: List<Int>): List<Int> {
+        require(stack.size >= 2)
+        return stack.dropLast(2) + stack.takeLast(2).reversed()
+    }
+
+    fun over(stack: List<Int>): List<Int> {
+        require(stack.size >= 2)
+        return stack + stack.takeLast(2).first()
+    }
+
+    private fun mapFun(operators: MutableMap<String, STACK_FUN>, word: String): STACK_FUN {
+        val num = word.toIntOrNull()
+        return if (num != null) {
+             { it + num }
+        } else {
+            operators.get(word.toLowerCase())?: :: throwError
+        }
+    }
+
+     fun throwError(stack: List<Int>) : List<Int> {
+        require(false);
+        return emptyList()
+    }
+
+    fun userDefine(funs: List<STACK_FUN>): STACK_FUN = funs.reduce { acc, fn -> { fn(acc(it)) } }
 }
+
